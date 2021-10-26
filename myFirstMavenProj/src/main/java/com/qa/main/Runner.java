b5eae17efcd9a0e64b0663773226d97a7d85b667
Daniil Murashev <daniil.murashev@gmail.com>
2021-10-26 01:09:38 +0100

    jar with arguments. Calc

--- myFirstMavenProj/src/main/java/com/qa/main/Runner.java
+++ myFirstMavenProj/src/main/java/com/qa/main/Runner.java
@@ -3,5 +3,32 @@ package com.qa.main;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Heylow");
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int res;
+        switch (args[2]){
+            case "add":
+                res = num1+num2;
+                System.out.println(num1 +" + " + num2 + " = " + res);
+                break;
+            case "del":
+                res = num1-num2;
+                System.out.println(num1 +" - " + num2 + " = " + res);
+                break;
+            case "mul":
+                res = num1*num2;
+                System.out.println(num1 +" * " + num2 + " = " + res);
+                break;
+            case "div":
+                res = num1/num2;
+                System.out.println(num1 +" / " + num2 + " = " + res);
+                break;
+            default:
+                System.out.println("enter operation type");
+                break;
+        }
+
+
+
     }
 }
