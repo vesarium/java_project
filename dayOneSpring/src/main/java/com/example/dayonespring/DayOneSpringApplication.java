03c0b3d7ee638526778244039dfcb86ef4844268
Daniil Murashev <daniil.murashev@gmail.com>
2021-11-08 23:04:42 +0000

    Device and User classes added

---
+++ dayOneSpring/src/main/java/com/example/dayonespring/DayOneSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.dayonespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayOneSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayOneSpringApplication.class, args);
+	}
+
+}
---
+++ dayOneSpring/src/test/java/com/example/dayonespring/DayOneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dayonespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayOneSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
